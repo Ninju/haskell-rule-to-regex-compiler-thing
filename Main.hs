619db102160fa55cd20ba4ceb73aa87e6424dfd1
Alex <alex.watt@me.com>
2009-03-25 17:25:24 +0000

    Added Main.hs

---
+++ Main.hs
@@ -0,0 +1,13 @@
+module Main where
+import PatternParser (parsePattern)
+import Control.Monad (forM_)
+
+main = forM_ patterns $ \pattern -> putStrLn $ pattern ++ " -> " ++ show (parsePattern pattern)
+
+patterns :: [String]
+patterns = [ "AAAA",
+             "AAA_#####",
+             "AAA_#######_##",
+             "A####_@@",
+             "??_AAA#A_#",
+             "@@@@_####_????" ]
