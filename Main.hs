836e924624f39580de83212ef6649caebdafb2c8
Alex <alex.watt@me.com>
2009-03-26 10:17:22 +0000

    added type signature for main

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 import PatternParser (parsePattern)
 import Control.Monad (forM_)
 
+main :: IO ()
 main = forM_ patterns $ \pattern -> putStrLn $ pattern ++ " -> " ++ show (parsePattern pattern)
 
 patterns :: [String]
